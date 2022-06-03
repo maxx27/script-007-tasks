052fc50eb7bc19a3e482f1661a5edd75ac3a83d1
Maxim Suslov <suslov.mn@gmail.com>
2022-06-03 21:24:54 +0300

    Add 03_logging_solution

--- README.md
+++ README.md
@@ -125,7 +125,7 @@ $ git merge trainer/BRANCH_NAME
 - [x] Specify directory to keep manage files via CLI arguments
 - [x] Cover functionality using `pytest`
 - [ ] Deploy via Docker image (for those who is familiar with Docker)
-- [ ] Use `logging` module for logging
+- [x] Use `logging` module for logging
 
 ## File Service
 
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -42,3 +42,38 @@ def test_main_data_dir_param(param, tmp_path):
 
     assert exitcode == 0
     assert os.path.exists(adir)
+
+
+@pytest.mark.parametrize('param', ['-l', '--log-file'])
+def test_main_default_log_created(param, tmp_path):
+    fn = tmp_path / 'server.log'
+    assert not fn.exists()
+    capture_main([param, str(fn)])
+    assert fn.exists()
+
+
+def test_main_log_info():
+    out, err, exitcode = capture_main(['--log-level', 'info'])
+    assert exitcode == 0
+    assert len(out) == 0
+    assert b"DEBUG in main" not in err
+
+
+def test_main_log_debug():
+    out, err, exitcode = capture_main(['--log-level', 'debug'])
+    assert exitcode == 0
+    assert len(out) == 0
+    assert b"DEBUG in main" in err
+
+
+@pytest.mark.xfail(reason='log is always of debug verbosity')
+def test_main_log_info_file(tmp_path):
+    fn = tmp_path / 'server.log'
+    capture_main(['--log-level', 'info', '--log-file', str(fn)])
+    assert b"DEBUG in main" not in fn.read_bytes()
+
+
+def test_main_log_debug_file(tmp_path):
+    fn = tmp_path / 'server.log'
+    capture_main(['--log-level', 'debug', '--log-file', str(fn)])
+    assert b"DEBUG in main" in fn.read_bytes()
